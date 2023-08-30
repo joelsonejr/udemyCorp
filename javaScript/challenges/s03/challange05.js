052c016c8651b060223ffd95c96f3be94821451c
Joelson <jo3rn4l@pop-os.localdomain>
2023-08-30 18:28:37 -0300

    organized course files, and developed challenge #5

--- javaScript/challenges/s03/challange05.js
+++
@@ -1,7 +0,0 @@
-score01 = 10;
-
-const calcAverage = (score01, score02, score03) => (score01 + score02 + score03) / 3;
-
-const result01 = calcAverage(60, 30, 30)
-
-console.log(`The result was ${result01}`);
